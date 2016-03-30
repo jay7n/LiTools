7514e7a589977bfc11e8ec4b37bba3bdc0da1c70
lookof <lookof@gmail.com>
2016-03-31 06:26:21 +0800

    new litools: liwebspider created

---
+++ liwebspider/liwebspiderconf.py
@@ -0,0 +1,26 @@
+GrabHtmlContent = {
+    'MaxTryCount': 5
+}
+
+HitTemplate = {
+    'Meta': '''
+    ''',
+
+    'Element': '''
+        <ul class="mnlt">
+                <li class="left" id="imgshow">
+                    <p></p>
+                    <p>
+                        <img alt="$TEXT_1$" src="$IMG_1$" style="float:none;" title="$TEXT_2$">
+                    </p>
+                    <p></p>
+                </li>
+        </ul>
+    '''
+}
+
+Debug = {
+    'FilterHtmlContent': {
+        'LocalFileName': 'try_this'
+    }
+}
